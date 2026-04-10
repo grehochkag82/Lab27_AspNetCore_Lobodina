880ddf785742c25e360508be63cb8b821ba0a30a
grehochka82 <lobodina0607@mail.ru>
2026-04-10 09:31:51 +0300

    Step 6: multiple routes with parameters

--- WebServer/Program.cs
+++ WebServer/Program.cs
@@ -7,5 +7,27 @@ app.MapGet("/", () => "Добро пожаловать на сервер !");
 app.MapGet("/about", () => "Это мой первый ASP.NET Core сервер");
 app.MapGet("/time", () => $"Время на сервере: {DateTime.Now}");
 app.MapGet("/hello/{name}", (string name) => $"Привет,{name}!");
-app.MapGet("/sum/{a}/{b}", (int a, int b) => $"Итог,{int a, int b}!");
+//app.MapGet("/sum/{a}/{b}", (int a, int b) => $"Итог,{int a, int b}!");
+
+app.MapGet("/student", () => new {
+    Name="Вероника Лободина",
+    Group="ИСП-232",
+    Year=3,
+    IsActive=true
+});
+app.MapGet("/subjects", () => new[] {
+    "РПМ",
+    "РМП",
+    "ИСРПО",
+    "СП",
+});
+
+app.MapGet("/product/{id}", (int id) => new Product(
+    Id:id,
+    Name:$"Товар #{id}",
+    Price: id * 99.99m,
+    InStock: id %2==0
+));
 app.Run();
+
+record Product(int Id, string Name, decimal Price, bool InStock);
