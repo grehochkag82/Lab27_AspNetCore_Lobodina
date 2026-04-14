01b96ff924ecbfa57b7c53bbb71a9dda44175ed5
grehochka82 <lobodina0607@mail.ru>
2026-04-14 14:04:55 +0300

    Step 8: middleware logging and custom headers

--- WebServer/Program.cs
+++ WebServer/Program.cs
@@ -3,6 +3,17 @@ using System.Data;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+app.Use(async(context, next) => {
+    Console.WriteLine($"[LOG]{context.Request.Method} {context.Request.Path}");
+    await next(context);
+    Console.WriteLine($"[LOG] Ответ получен; {context.Response.StatusCode}");
+});
+
+
+app.Use(async(context, next) => {
+    context.Response.Headers.Append("X-Powered-By","ASP.NET Core Lab27");
+    await next(context);
+});
 app.MapGet("/", () => "Добро пожаловать на сервер !");
 app.MapGet("/about", () => "Это мой первый ASP.NET Core сервер");
 app.MapGet("/time", () => $"Время на сервере: {DateTime.Now}");
@@ -28,6 +39,24 @@ app.MapGet("/product/{id}", (int id) => new Product(
     Price: id * 99.99m,
     InStock: id %2==0
 ));
+
+app.Use(async (context, next) =>
+{
+    var key = context.Request.Query["key"];
+
+    if (key != "secret")
+    {
+        context.Response.StatusCode = 401;
+        await context.Response.WriteAsync("Unauthorized: invalid or missing 'key' parameter");
+        return; 
+    }
+
+    await next(context);
+});
+
+app.MapGet(" ", () => "lalala");
+
+
 app.Run();
 
 record Product(int Id, string Name, decimal Price, bool InStock);
